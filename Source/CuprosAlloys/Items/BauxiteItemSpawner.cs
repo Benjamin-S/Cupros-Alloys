e2eb73d04e95778bb43830c3719b18f4bb762067
cuproPanda <cupropanda@gmail.com>
2017-05-21 01:59:22 -0400

    Updated to A17 - new ores, new alloys, new floors, EXP integration, refactored research, item spawners

---
+++ Source/CuprosAlloys/Items/BauxiteItemSpawner.cs
@@ -0,0 +1,21 @@
+﻿using Verse;
+
+namespace CuprosAlloys {
+
+  internal class BauxiteItemSpawner : ItemSpawner {
+    
+    public override void SpawnSetup(Map map, bool respawningAfterLoad) {
+      base.SpawnSetup(map, respawningAfterLoad);
+
+      // Give a 5% chance to spawn quartz, if it exists
+      if (Rand.Range(0, 100) < 5) {
+        if (DefDatabase<ThingDef>.GetNamed("EXP_Quartz", false) != null) {
+          SpawnRandomQuantity(ThingDef.Named("EXP_Quartz"), 1, 5);
+        }
+      }
+
+      SpawnRandomQuantity(ThingDef.Named("CAL_Aluminum"), 15, 25);
+      Destroy();
+    }
+  }
+}
---
+++ Source/CuprosAlloys/Items/GalenaItemSpawner.cs
@@ -0,0 +1,28 @@
+﻿using Verse;
+
+namespace CuprosAlloys {
+
+  internal class GalenaItemSpawner : ItemSpawner {
+
+    public override void SpawnSetup(Map map, bool respawningAfterLoad) {
+      base.SpawnSetup(map, respawningAfterLoad);
+			
+			int chance = Rand.Range(0, 100);
+			
+			// Give a 5% chance to spawn cerussite, if it exists
+			if (chance < 5){
+				if (DefDatabase<ThingDef>.GetNamed("EXP_Cerussite", false) != null){
+					SpawnExactQuantity(ThingDef.Named("EXP_Cerussite"), 1);
+				}
+			}
+
+      // Give a 25% chance to spawn silver
+      if (chance < 25) {
+        SpawnRandomQuantity(ThingDef.Named("Silver"), 10, 15);
+      }
+
+      SpawnRandomQuantity(ThingDef.Named("EXP_Lead"), 10, 20);
+      Destroy();
+    }
+  }
+}
